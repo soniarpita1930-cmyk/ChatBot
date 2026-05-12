4ad49042c874ed0cbc2313a4c18dba56a6a1e506
Arpitaa_soni01 <soniarpita1930@gmail.com>
2026-05-12 19:59:07 +0530

    Added CodSoft Java Projects

--- ChatBot.java
+++ ChatBot.java
@@ -1,38 +1,19 @@
 
 import java.util.Scanner;
 
-public class ChatBot {
+class ChatBot {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String msg = sc.nextLine();
 
-        System.out.println("ChatBot Started!");
-        System.out.println("Type 'bye' to exit.\n");
-
-        while (true) {
-
-            System.out.print("You: ");
-            String msg = sc.nextLine().toLowerCase();
-
-            if (msg.equals("hi") || msg.equals("hello")) {
-                System.out.println("Bot: Hey!");
-
-            } else if (msg.equals("how are you")) {
-                System.out.println("Bot: I'm fine, thanks!");
-
-            } else if (msg.equals("your name")) {
-                System.out.println("Bot: I'm a Java ChatBot.");
-
-            } else if (msg.equals("bye")) {
-                System.out.println("Bot: Goodbye");
-                break;
-
-            } else {
-                System.out.println("Bot: Sorry, I don't understand.");
-            }
+        if (msg.equals("hi")) {
+            System.out.println("Hello!"); 
+        }else if (msg.equals("how are you")) {
+            System.out.println("I am fine"); 
+        }else {
+            System.out.println("Okay");
         }
-
-        sc.close();
     }
 }
